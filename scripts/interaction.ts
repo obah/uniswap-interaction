c4c5dc5be5d7e1b975e3f3b4e6ccc772ecbc8f18
obah <olusojiobah@gmail.com>
2024-09-17 18:02:40 +0100

    added screenshot

--- scripts/interaction.ts
+++ scripts/interaction.ts
@@ -1,69 +1,65 @@
-// import helpers from "@nomicfoundation/hardhat-network-helpers";
 import { ethers } from "hardhat";
 const helpers = require("@nomicfoundation/hardhat-network-helpers");
 
 async function main() {
-  const USDT = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
-  const TOKEN_HOLDER = "0x28C6c06298d514Db089934071355E5743bf21d60";
-  const UNISWAP_V2_ROUTER = "0xf164fC0Ec4E93095b804a4795bBe1e041497b92a";
-  const POOL_ADDRESS = "0x7858E59e0C01EA06Df3aF3D20aC7B0003275D4Bf";
+  const ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+  const USDT = "0x6033F7f88332B8db6ad452B7C6D5bB643990aE3f";
 
-  const USDTDesired = ethers.parseUnits("1000", 18);
-  const USDTMin = ethers.parseUnits("50", 18);
-  const ETHMin = ethers.parseEther("0.01");
-  const ETHDesired = ethers.parseEther("2");
-  const deadline = Math.floor(Date.now() / 1000) + 60 * 10; //30 Minutes in UNIX Timestamp
+  const TOKEN_HOLDER = "0x28C6c06298d514Db089934071355E5743bf21d60";
 
   await helpers.impersonateAccount(TOKEN_HOLDER);
-  const impersonatedSigner = await ethers.getSigner(TOKEN_HOLDER);
+  const impersonateSigner = await ethers.getSigner(TOKEN_HOLDER);
 
-  const USDTContract = await ethers.getContractAt(
+  const designerAmountA = ethers.parseUnits("1000", 18);
+  const minAmountA = ethers.parseUnits("10", 18);
+
+  const amountEthMin = ethers.parseEther("5");
+
+  const ETHDesired = ethers.parseEther("6");
+
+  const deadLine = Math.floor(Date.now() / 1000) + 60 * 60;
+
+  const USDT_Contract = await ethers.getContractAt(
     "IERC20",
     USDT,
-    impersonatedSigner
+    impersonateSigner
   );
 
   const ROUTER = await ethers.getContractAt(
     "IUniswapV2Router01",
-    UNISWAP_V2_ROUTER,
-    impersonatedSigner
+    ROUTER_ADDRESS,
+    impersonateSigner
   );
 
-  await USDTContract.approve(ROUTER, USDTDesired);
+  await USDT_Contract.approve(ROUTER, designerAmountA);
 
-  const USDTBalanceBeforeTransaction = await USDTContract.balanceOf(
-    TOKEN_HOLDER
-  );
-  //   const ETHBeforeTransaction = await helpers
+  const usdtBal = await USDT_Contract.balanceOf(impersonateSigner.address);
+  const ethBal = await ethers.provider.getBalance(impersonateSigner.address);
 
   console.log(
     "======================BEFORE ADDING LIQUIDITY======================="
   );
-  console.log("USDT IN WALLET:", USDTBalanceBeforeTransaction);
-  console.log("ETH IN WALLER");
+  console.log("LISK IN WALLET:", usdtBal);
 
-  await USDTContract.approve(ROUTER, USDTDesired);
-
-  const tx = await ROUTER.addLiquidityETH(
+  await ROUTER.addLiquidityETH(
     USDT,
-    USDTDesired,
-    USDTMin,
-    ETHMin,
-    impersonatedSigner,
-    deadline,
+    designerAmountA,
+    minAmountA,
+    amountEthMin,
+    TOKEN_HOLDER,
+    deadLine,
     { value: ETHDesired }
   );
 
-  await tx.wait();
-
-  const USDTBalanceAfterTransaction = await USDTContract.balanceOf(
-    TOKEN_HOLDER
+  const usdtBalAfter = await USDT_Contract.balanceOf(impersonateSigner.address);
+  const ethBalAfter = await ethers.provider.getBalance(
+    impersonateSigner.address
   );
 
   console.log(
     "======================AFTER ADDING LIQUIDITY======================="
   );
-  console.log("USDT IN WALLET:", USDTBalanceAfterTransaction);
+  console.log("LISK IN WALLET:", usdtBalAfter);
 }
 
 main().catch((error) => {
